555324785a154bff783cf73b339d94e86ffa2820
crypto-jones <integral.being@gmail.com>
2018-10-16 14:12:51 -0700

    Set up knex and sqlite3

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const helmet = require('helmet');
+const knex = require('knex');
+
+const knexConfig = require('./knexfile.js');
+const db = knex(knexConfig.development);
 
 const server = express();
 
@@ -7,6 +11,18 @@ server.use(express.json());
 server.use(helmet());
 
 // endpoints here
+server.post('/api/zoos', (req, res) => {
+  const name = req.body;
+
+  db.insert(name)
+    .into('zoos')
+    .then(id => {
+      res.status(201).json(id);
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+});
 
 const port = 3300;
 server.listen(port, function() {
