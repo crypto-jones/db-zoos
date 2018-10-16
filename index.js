5de8ded48566b49fe804018a068b94317ebf088c
crypto-jones <integral.being@gmail.com>
2018-10-16 15:27:16 -0700

    Create all required endpoints

--- index.js
+++ index.js
@@ -11,6 +11,8 @@ server.use(express.json());
 server.use(helmet());
 
 // endpoints here
+
+// Create Zoos
 server.post('/api/zoos', (req, res) => {
   const name = req.body;
 
@@ -22,6 +24,69 @@ server.post('/api/zoos', (req, res) => {
     .catch(err => {
       res.status(500).json(err);
     });
+});
+
+// Get All Zoos
+server.get('/api/zoos', (req, res) => {
+  db('zoos')
+    .then(zoos => {
+      res.status(200).json(zoos);
+    })
+    .catch(err => {
+      res.status(500).json(err);
+    });
+});
+
+// Get Zoo By ID
+server.get('/api/zoos/:id', async (req, res) => {
+  try {
+    const { id } = req.params;
+    const zoo = await db('zoos')
+      .where({ id })
+      .first();
+
+    if (zoo) {
+      res.status(200).json(zoo);
+    } else {
+      res.status(404).json({ message: 'Zoo not found' });
+    }
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// Update Zoo
+server.put('/api/zoos/:id', (req, res) => {
+  const { id } = req.params;
+  const changes = req.body;
+
+  db('zoos')
+    .where({ id })
+    .update(changes)
+    .then(count => {
+      if (!count || count < 1) {
+        res.status(404).json({ message: 'No records founds to update' });
+      }
+      res.status(200).json(count);
+    })
+    .catch(err => res.status(500).json(err));
+});
+
+// Delete Zoo
+server.delete('/api/zoos/:id', (req, res) => {
+  const { id } = req.params;
+  const changes = req.body;
+
+  db('zoos')
+    .where({ id })
+    .del(changes)
+    .then(count => {
+      if (!count || count < 1) {
+        res.status(404).json({ message: 'No records founds to delete' });
+      }
+      res.status(200).json(count);
+    })
+    .catch(err => res.status(500).json(err));
 });
 
 const port = 3300;
